429e2c452e227fa65c90d68a167b03fe3c3089d1
Yasmin Tiomkin <Yasmin.Tiomkin@gmail.com>
2020-09-09 23:29:21 +0300

    data source

---
+++ Assets/Surprise/SRDataSource.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SRDataSource
+{
+    static public SRGameData gameData = new SRGameData();
+
+    static private string filename = Application.persistentDataPath + "/GameData.json";
+
+    static public void Save()
+    {
+        string val = JsonUtility.ToJson(gameData);
+        System.IO.File.WriteAllText(filename, val);
+    }
+
+    static public void Load()
+    {
+        //if (isDataSaved)
+        try
+        {
+            string val = System.IO.File.ReadAllText(filename);
+            var data = JsonUtility.FromJson<SRGameData>(val);
+            Debug.Log(val);
+            if (data == null)
+            {
+                gameData = new SRGameData();
+            }
+            else
+            {
+                data.LoadPostProcessing();
+                gameData = data;
+            }
+        }
+        catch (Exception e)
+        {
+            // ignore error
+            Debug.Log("file not found");
+        }
+    }
+}
+
+[Serializable]
+public class SRGameData
+{
+    public List<SRSurpriseData> surprises = new List<SRSurpriseData>();
+
+    public void LoadPostProcessing()
+    {
+        // JsonUtility.FromJson does not call constructor
+        if (surprises == null)
+        {
+            surprises = new List<SRSurpriseData>();
+        }
+    }
+}
+
+
+[Serializable]
+public class SRSurpriseData
+{
+    public string spriteFilename;
+    public Vector3 position;
+    public Quaternion rotation;
+}
+
---
+++ Assets/Surprise/SRTapToPlaceObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SRTapToPlaceObject : MonoBehaviour
+{
+    public GameObject gameobjectToInstantiate;
+
+    private GameObject spawndObject;
+    private ARRaycastManager raycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out touchPosition))
+        {
+            return;
+        }
+
+        if (raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if (spawndObject == null)
+            {
+                spawndObject = Instantiate(gameobjectToInstantiate, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawndObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
